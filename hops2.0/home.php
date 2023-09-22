05c53cfe9ac68ab031bd11712840a9b440b99e6e
Christian <87219733+ikkisan20@users.noreply.github.com>
2023-09-22 12:05:36 +0800

    Update home.php

--- hops2.0/home.php
+++ hops2.0/home.php
@@ -12,7 +12,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hops Admin</title>
-    <link rel="stylesheet" href="style/style1.css">
+    <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
     <div class="nav">
