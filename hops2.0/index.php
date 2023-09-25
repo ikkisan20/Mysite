84567eb3d0becf45d822ec8c3d818da3799d8f0e
Christian <87219733+ikkisan20@users.noreply.github.com>
2023-09-25 20:51:54 +0800

    Add files via upload

--- hops2.0/index.php
+++ hops2.0/index.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,10 +9,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hops Admin</title>
     <link rel="stylesheet" href="style/style.css">
+    
 </head>
 <body>
-
-    <div class="nav">
+  <div class="nav">
         <div class="logo"><img src="./image/kirby.gif" alt="logo">HOPS..</div>
             <ul class="navItems">
                 <li><a href="#"></a></i></li>
@@ -23,41 +27,39 @@
         <div class="box form-box">
 
             <?php
+            
                 include("php/config.php");
                 if(isset($_POST['submit'])){
-                    $username = $_POST['username'];
-                    $email = $_POST['email'];
-                    $password = $_POST['password'];
+                    $email = mysqli_real_escape_string($con,$_POST['email']);
+                    $password = mysqli_real_escape_string($con,$_POST['password']);
 
-                    //verify email
+                    //result email
 
-                    $verify_query = mysqli_query($con,"SELECT Email FROM users WHERE Email='$email'");
+                    $result = mysqli_query($con,"SELECT * FROM users WHERE Email='$email' AND Password='$password' ") or die("Select Error");
+                    $row = mysqli_fetch_assoc($result);
 
-                    if(mysqli_num_rows($verify_query) !=0){
-                        echo "<div class='message'>
-                            <p>This email is used, Try another One Please!</p>
-                            </div> <br>";
-                        echo "<a href='javascript:self.history.back()'><button class='btn'>Go Back</button>";
+                    if(is_array($row) && !empty($row)){
+                        $_SESSION['valid'] = $row['Email'];
+                        $_SESSION['username'] = $row['Username'];
+                        $_SESSION['age'] = $row['Age'];
+                        $_SESSION['id'] = $row['Id'];
                     }
                     else{
-
-                        mysqli_query($con, "INSERT INTO users(Username,Email,Password) VALUES('$username','$email','$password')") or die("Error Occured");
-
                         echo "<div class='message'>
-                            <p>Registration successfully!</p>
-                            </div> <br>";
-                        echo "<a href='index.php'><button class='btn'>Login Now</button>";
+                        <p>Wrong username or Password</p>
+                        </div> <br>";
+                        echo "<a href='index.php'><button class='btn'>Go Back</button>";
+                    }
+                    if(isset($_SESSION['valid'])){
+                        header("Location: home.php");
                     }
                 }else{
+
+            
             ?>
             <header><span class="multiText"></span></header>
             <form action="" method="post">
                 <!--Username-->
-                <div class="field input">
-                    <label for="username">Username</label>
-                    <input type="text" name="username" id="username" autocomplete="off" required>
-                </div>
-                <!--Email-->
                 <div class="field input">
                     <label for="email">Email</label>
                     <input type="text" name="email" id="email" autocomplete="off" required>
@@ -69,10 +71,10 @@
                 </div>
                 <!--Submit-->
                 <div class="field input">
-                    <input type="submit" class="btn" name="submit" value="Register" required>
+                    <input type="submit" class="btn" name="submit" value="Login" required>
                 </div>
                 <div class="links">
-                    Already have an account? <a href="index.php">Sign In</a>
+                    Don't have an account? <a href="register.php">Sign up now</a>
                 </div>
             </form>
         </div>
@@ -80,16 +82,36 @@
     </div>
 
     <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
-    
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     
     <script>
         var typingEffect = new Typed(".multiText",{ 
-            strings : ["Sign Up"],
+            strings : ["Login"],
             loop : true,
             typeSpeed : 100,
             backSpeed: 80,
             backDelay : 1500, 
         })
+
+        $(document).on('submit', '#loginForm', function (e) {
+            e.preventDefault();
+
+            $.ajax({
+                method: "POST",
+                url: "index.php",
+                data: $(this).serialize(),
+                success: function (data) {
+
+                    if (data === 'success') {
+                        window.location.href = "home.php";
+                    } else {
+                        $('#msg').html(data);
+                        $('#loginForm').find('input').val('');
+                    }
+
+                }
+            });
+        });
     </script>
 
 </body>
